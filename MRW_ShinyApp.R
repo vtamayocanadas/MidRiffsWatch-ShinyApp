0facaa8ec6ea7436fd5ccb58ac8b4648edefff2f
Cheyenne <cccoxon@ucsb.edu>
2019-02-22 09:33:45 -0800

    change title of app

--- MRW_ShinyApp.R
+++ MRW_ShinyApp.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Midriff Watch Tool"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
