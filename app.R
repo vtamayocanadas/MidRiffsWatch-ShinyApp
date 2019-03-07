58cfb05a9eae9da0717b1e7ce0b9341c76ea0edd
Valeria Tamayo Canadas <vtamayo@bren.ucsb.edu>
2019-03-06 16:31:46 -0800

    added the dropdown menu for the fisheries

--- app.R
+++ app.R
@@ -13,10 +13,12 @@ library(shiny)
 ui <- fluidPage(
   fluidRow(
   tags$h1("Midriff Watch Tool"),
-  p("This App will provide users with a two scenario world in the mid-Gulf of California: one scenario where there are no Marine Reserves and one with 5% of the area protected as a net of Marine Reserves. The App will provide information on 13 fisheries that account for approximately 90% catch in the region. Using the model developed by the Midriff Watch Group Project Team, users will be able to visualize both observational data from 2005 to 2015, and projected outputs up to 2105. They will have the option to choose from biomass and revenues from catch, derived from those fisheries. All outputs will allow users to compare two scenarios: Marine Reserves implemented, and Marine Reserves not implemented.")
+  p("This App shows a two scenario world in the mid-Gulf of California: one scenario where there are no Marine Reserves and one with 5% of the area protected as a net of Marine Reserves. The App will provide information on 13 fisheries that account for approximately 90% catch in the region. Using the model developed by the Midriff Watch Group Project Team, users will be able to visualize both observational data from 2005 to 2015, and projected outputs up to 2105. They will have the option to choose from biomass and revenues from catch, derived from those fisheries. All outputs will allow users to compare two scenarios: Marine Reserves implemented, and Marine Reserves not implemented.")
   ),
   title = "Options",
-  navlistPanel(
+  navbarPage(title = "Options", 
+             navlistPanel(
+    navbarMenu(title = "Fisheries", tabPanel("Atrina tuberculosa"), tabPanel("Callinectes Bellicosus"), tabPanel("Cephalopolis cruentata"), tabPanel("Dasyatis dipterura"), tabPanel("Epinephelus acanthistius"), tabPanel("Lutjanus argentiventris"), tabPanel("Cynoscion othonopterus"), tabPanel("Mugil curema"), tabPanel("Octopus bimaculatus"), tabPanel("Panilurus Inflatus"), tabPanel("Scomberomorus maculatus"), tabPanel("Sphoeroides annulatus"), tabPanel("Squatina californica")),
     tabPanel(title = "Biomass"),
     tabPanel(title = "Catch"),
     tabPanel("Profit")),
@@ -32,6 +34,7 @@ ui <- fluidPage(
       plotOutput("hist")
     )
   )
+)
 )
   
  
